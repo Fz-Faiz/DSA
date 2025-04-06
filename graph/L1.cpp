b6415ad038eddc970b8a518a16faf6c4ad6915fb
Faiz Ahmad <faizahmad43430@gmail.com>
2025-04-06 16:34:38 +0530

    graph2

--- graph/L1.cpp
+++ graph/L1.cpp
@@ -1,9 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void dfs(int node, vector<vector<int>>& adj, vector<int> &vis, vector<int> &ans){
+    vis[node]=1;
+    ans.push_back(node);
+    for(auto it: adj[node]){
+        if(!vis[it]){
+            dfs(it, adj, vis, ans);
+        }
+    }
+}
+
 int main() {
-    int n ,m;
-    cin >> n >> m;
+    // int n ,m;
+    // cin >> n >> m;
 
     // // matrix method
     // // o(n*n) space 
@@ -25,4 +35,37 @@ int main() {
     //     adj[v].push_back(u);
 
     // }
+
+    // BFS
+
+    // int visited[n] ={0};
+    // visited[0]=1;
+    // queue<int> q;
+    // q.push(0);
+    // vector<int> bfs;
+    // while(!q.empty()) {
+    //     int node = q.front();
+    //     q.pop();
+    //     bfs.push_back(node);
+
+    //     for(auto it: adj[node]){
+    //         if(!visited[it]){
+    //             visited[it] = 1;
+    //             q.push(it)
+    //         }
+    //     }
+    // }
+    // return bfs;
+
+
+    // // dfs recursion
+
+    // int n = adj.size();
+    //     vector<int> vis(n,0);
+    //     int start =0;
+    //     vector<int> ans;
+    //     dfs(start, adj, vis, ans);
+    //     return ans;
+    
+
 }
