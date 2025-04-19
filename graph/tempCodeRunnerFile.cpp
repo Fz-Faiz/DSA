0379e241d61c11e34792e999a00ff3beb68069bb
Faiz Ahmad <faizahmad43430@gmail.com>
2025-04-19 22:33:55 +0530

    graph4

---
+++ graph/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+nt vis[V] = {0};
+		int pathVis[V] = {0};
+
+		for (int i = 0; i < V; i++) {
+			if (!vis[i]) {
+				if (dfsCheck(i, adj, vis, pathVis) == true) return true;
+			}
+		}
+		return false;
