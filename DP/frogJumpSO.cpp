5075a08eca754274bdccdb26eb7c7f03e77b38bf
Faiz Ahmad <faizahmad43430@gmail.com>
2025-05-23 21:46:29 +0530

    dp3

---
+++ DP/frogJumpSO.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+
+    int minCost(vector<int>& height) {
+        // Code here
+            int n = height.size();
+        int prev = 0;
+        int prev2 = 0;
+        for(int i=1 ; i<n;i++){
+            int fs = prev + abs(height[i] - height[i-1]);
+            int ss = INT_MAX;
+            if(i > 1) ss = prev2 + abs(height[i] - height[i-2]);
+            int curr = min(fs, ss);
+            prev2 = prev;
+            prev = curr;
+
+        }   
+
+        return prev; 
+    }
+    
+};
